5122953ebf0dd981f9bb959ff61964bac8d8d128
julielblatt <85769226+julielblatt@users.noreply.github.com>
2025-04-09 12:14:48 -0300

    Create Codigo_teste.py

---
+++ Analise Sintatica/Codigo_teste.py
@@ -0,0 +1,25 @@
+// PRIMEIRO CODIGO
+
+if (x + 2) {
+    return x;
+}
+y = 5;
+return y + 1;
+
+// SEGUNDO CODIGO
+
+x = 0;
+while (x < 10) {
+    x = x + 1;
+    y = y * 2;
+}
+return x;
+
+// TERCEIRO CODIGO
+
+for (i = 0; i < 10; i = i + 1) {
+    if (i * 2) {
+        y = y + i;
+    }
+}
+return y;
