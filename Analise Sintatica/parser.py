61908f4e6a4e300597559f4bda6c5b98b5ebeb7c
julielblatt <85769226+julielblatt@users.noreply.github.com>
2025-04-23 20:11:17 -0300

    Update parser.py

--- Analise Sintatica/parser.py
+++ Analise Sintatica/parser.py
@@ -25,7 +25,7 @@ class Parser:
 
     def cmd_list(self):
         comandos = []
-        while self.token_atual()[0] in ['KEYWORD', 'ID']:
+        while self.token_atual()[0] in ['KEYWORD', 'ID'] or self.token_atual()[1] == 'func':  # Modificado para incluir 'func'
             comandos.append(self.cmd())
         return {'type': 'CMD_LIST', 'body': comandos}
 
@@ -34,9 +34,9 @@ class Parser:
 
         if val == 'if':
             self.consumir('KEYWORD')
-            self.consumir('SYMBOL')  
+            self.consumir('SYMBOL')  # (
             cond = self.exp()
-            self.consumir('SYMBOL') 
+            self.consumir('SYMBOL')  # )
             bloco = self.bloco()
             return {'type': 'IF', 'cond': cond, 'body': bloco}
 
@@ -50,33 +50,48 @@ class Parser:
 
         elif val == 'for':
             self.consumir('KEYWORD')
-            self.consumir('SYMBOL')  
+            self.consumir('SYMBOL')  # (
             init = self.atribuicao()
-            self.consumir('SYMBOL')  
+            self.consumir('SYMBOL')  # ;
             cond = self.exp()
-            self.consumir('SYMBOL')  
+            self.consumir('SYMBOL')  # ;
             update = self.atribuicao()
-            self.consumir('SYMBOL')  
+            self.consumir('SYMBOL')  # )
             bloco = self.bloco()
             return {'type': 'FOR', 'init': init, 'cond': cond, 'update': update, 'body': bloco}
 
         elif val == 'return':
             self.consumir('KEYWORD')
             expr = self.exp()
-            self.consumir('SYMBOL')  
+            self.consumir('SYMBOL')  # ;
             return {'type': 'RETURN', 'value': expr}
 
+        elif val == 'func':  # Alterado para reconhecer 'func' como palavra-chave
+            self.consumir('KEYWORD')  # A palavra 'func' é uma KEYWORD
+            nome = self.consumir('ID')
+            self.consumir('SYMBOL')  # (
+            params = self.param_list()
+            self.consumir('SYMBOL')  # )
+            body = self.bloco()
+            return {'type': 'FUNC_DECL', 'name': nome, 'params': params, 'body': body}
+
         elif tipo == 'ID':
-            atrib = self.atribuicao()
-            self.consumir('SYMBOL')  
-            return {'type': 'ASSIGN', **atrib}
+            lookahead = self.tokens[self.pos + 1][1] if self.pos + 1 < len(self.tokens) else ''
+            if lookahead == '(':
+                func_call = self.factor()
+                self.consumir('SYMBOL')  # ;
+                return {'type': 'FUNC_CALL_STMT', 'call': func_call}
+            else:
+                atrib = self.atribuicao()
+                self.consumir('SYMBOL')  # ;
+                return {'type': 'ASSIGN', **atrib}
 
         else:
             raise SyntaxError(f"Comando inválido: {val}")
 
     def atribuicao(self):
         var = self.consumir('ID')
-        op = self.consumir('OP')  
+        op = self.consumir('OP')
         expr = self.exp()
         return {'var': var, 'op': op, 'value': expr}
 
@@ -102,8 +117,14 @@ class Parser:
             self.consumir('NUMBER')
             return {'type': 'NUMBER', 'value': int(val)}
         elif tipo == 'ID':
-            self.consumir('ID')
-            return {'type': 'ID', 'name': val}
+            nome = self.consumir('ID')
+            if self.token_atual()[1] == '(':
+                self.consumir('SYMBOL')
+                args = self.arg_list()
+                self.consumir('SYMBOL')
+                return {'type': 'FUNC_CALL', 'name': nome, 'args': args}
+            else:
+                return {'type': 'ID', 'name': nome}
         elif val == '(':
             self.consumir('SYMBOL')
             expr = self.exp()
@@ -113,7 +134,25 @@ class Parser:
             raise SyntaxError(f"Fator inválido: {val}")
 
     def bloco(self):
-        self.consumir('SYMBOL')  
+        self.consumir('SYMBOL')  # {
         comandos = self.cmd_list()
-        self.consumir('SYMBOL') 
+        self.consumir('SYMBOL')  # }
         return comandos
+
+    def param_list(self):
+        params = []
+        if self.token_atual()[0] == 'ID':
+            params.append(self.consumir('ID'))
+            while self.token_atual()[1] == ',':
+                self.consumir('SYMBOL')
+                params.append(self.consumir('ID'))
+        return params
+
+    def arg_list(self):
+        args = []
+        if self.token_atual()[0] != 'SYMBOL' or self.token_atual()[1] != ')':
+            args.append(self.exp())
+            while self.token_atual()[1] == ',':
+                self.consumir('SYMBOL')
+                args.append(self.exp())
+        return args
