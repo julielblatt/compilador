4e6ebc82a7f06c04d56c3ad6fbb5f32ad0e74675
julielblatt <85769226+julielblatt@users.noreply.github.com>
2025-04-30 20:23:48 -0300

    Update gerador_tac.py

--- Gerador Tac/gerador_tac.py
+++ Gerador Tac/gerador_tac.py
@@ -1,26 +1,59 @@
-from parser import Parser
-from analisador_lexico import analisador_lexico
-from gerador_tac import GeradorTAC
-from analise_semantica import AnalisadorSemantico
-from analise_semantica import SymbolTableBuilder
+class GeradorTAC:
+    def __init__(self):
+        self.temp_count = 0
+        self.instrucoes = []
 
-codigo = """
-func soma(a, b, c) {
-    return a + b;
-}
-x = soma(2, 3);
-"""
+    def novo_temp(self):
+        self.temp_count += 1
+        return f"t{self.temp_count}"
 
-tokens = analisador_lexico(codigo)
-parser = Parser(tokens)
-ast = parser.parse()
+    def gerar(self, node):
+        tipo = node.get("type")
 
-builder = SymbolTableBuilder()
-tabela_simbolos = builder.build(ast)
-semantico = AnalisadorSemantico(builder)
-semantico.verificar(ast)
-semantico.imprimir_erros()
+        if tipo == "CMD_LIST":
+            for cmd in node["body"]:
+                self.gerar(cmd)
 
-tac = GeradorTAC()
-tac.gerar(ast)
-tac.imprimir()
+        elif tipo == "ASSIGN":
+            temp = self.gerar(node["value"])
+            self.instrucoes.append(f"{node['var']} = {temp}")
+
+        elif tipo == "RETURN":
+            temp = self.gerar(node["value"])
+            self.instrucoes.append(f"return {temp}")
+
+        elif tipo == "BIN_OP":
+            esquerda = self.gerar(node["left"])
+            direita = self.gerar(node["right"])
+            temp = self.novo_temp()
+            self.instrucoes.append(f"{temp} = {esquerda} {node['op']} {direita}")
+            return temp
+
+        elif tipo == "NUMBER":
+            return str(node["value"])
+
+        elif tipo == "ID":
+            return node["name"]
+
+        elif tipo == "FUNC_CALL":
+            args = [self.gerar(arg) for arg in node["args"]]
+            temp = self.novo_temp()
+            self.instrucoes.append(f"{temp} = call {node['name']}({', '.join(args)})")
+            return temp
+
+        elif tipo == "FUNC_CALL_STMT":
+            self.gerar(node["call"])
+
+        elif tipo == "FUNC_DECL":
+            self.instrucoes.append(f"func {node['name']}({', '.join(node['params'])})")
+            self.gerar(node["body"])
+            self.instrucoes.append(f"endfunc")
+
+    def imprimir(self):
+        print("Código TAC gerado:")
+        for instr in self.instrucoes:
+            print(instr)
+
+
+
+            
